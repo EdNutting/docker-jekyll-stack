8babb9e035657bccf67243b8b79251488ee68a95
Ed Nutting <edwardnutting@outlook.com>
2020-02-26 16:00:15 +0000

    Remove dead code/comments

--- test.sh
+++ test.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Note: `workspaceFolder` is the folder that contains the Dockerfile
-#       The name is inherited from VSCode meaning.
-#       See vscode-user-settings.json.example
-
 docker run ednutting/jekyll-haskell-stack:dev /bin/bash -c "stack --version"
